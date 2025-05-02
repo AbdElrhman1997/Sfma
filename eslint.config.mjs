b6921446721bb853515a0f064db460c903a02e34
AbdElrhman1973 <abdelrahman.m@fatoorah.sa>
2025-05-03 00:06:16 +0300

    test any type issue

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -15,6 +15,7 @@ const eslintConfig = [
   {
     rules: {
       "@typescript-eslint/no-unused-vars": "off", // ✅ Added to suppress unused variable warnings
+      "@typescript-eslint/no-explicit-any": "off",
     },
   },
 ];
